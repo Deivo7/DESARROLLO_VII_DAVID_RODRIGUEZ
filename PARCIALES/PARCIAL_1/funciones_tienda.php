5c694062d31819ae83baa9a3b0e167b551e9ec53
Deivo7 <104737399+Deivo7@users.noreply.github.com>
2024-09-03 18:52:56 -0500

    añadida funcion calcular_descuento

---
+++ PARCIALES/PARCIAL_1/funciones_tienda.php
@@ -0,0 +1,14 @@
+<?php
+function calcular_descuento($total_compra) {
+    if ($total_compra > 1000) {
+        return $total_compra * 0.15;
+    } else if ($total_compra > 500) {
+        return $total_compra * 0.10;
+    } else if ($total_compra >= 100) {
+        return $total_compra * 0.05;
+    } else {
+        return 0;
+    }
+}
+
+?>
