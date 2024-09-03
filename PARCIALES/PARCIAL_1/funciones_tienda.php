4cf8dc582c2cdc7a340c81faf110ce05fd5fb1b3
Deivo7 <104737399+Deivo7@users.noreply.github.com>
2024-09-03 18:56:00 -0500

    añadida funcion calcular_total

--- PARCIALES/PARCIAL_1/funciones_tienda.php
+++ PARCIALES/PARCIAL_1/funciones_tienda.php
@@ -15,4 +15,8 @@ function aplicar_impuesto($subtotal) {
     return $subtotal * 0.07;
 }
 
+function calcular_total($subtotal, $descuento, $impuesto) {
+    return $subtotal - $descuento + $impuesto;
+}
+
 ?>
